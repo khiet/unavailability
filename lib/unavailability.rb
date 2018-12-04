45a48cb9d58265f312833a60dc2f6441e24593a5
Khiet Le <khiet.le@ehochef.com>
2018-12-04 14:31:09 +0000

    add capabilities to included Model

--- lib/unavailability.rb
+++ lib/unavailability.rb
@@ -1,5 +1,21 @@
 require "unavailability/version"
 
 module Unavailability
-  # Your code goes here...
+  def self.included(base)
+    base.class_eval do
+      has_many :unavailable_dates, dependent: :destroy
+    end
+
+    scope :available_for_date, ->(date) do
+      where('NOT EXISTS (SELECT unavailable_dates.* FROM unavailable_dates WHERE unavailable_dates.user_id = users.id AND (unavailable_dates.from <= ? AND unavailable_dates.to >= ?))', date, date)
+    end
+  end
+
+  def available_for_date?(date)
+    unavailable_dates.each do |unavailable_date|
+      return false if unavailable_date.range.cover?(date)
+    end
+
+    true
+  end
 end
