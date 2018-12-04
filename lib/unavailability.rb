cd4bc81002d745e5ed66e880d230d316ba40888a
Khiet Le <khiet.le@ehochef.com>
2018-12-04 15:50:43 +0000

    fix module scope issue

--- lib/unavailability.rb
+++ lib/unavailability.rb
@@ -1,13 +1,15 @@
 require "unavailability/version"
 
+require 'active_record'
+
 module Unavailability
   def self.included(base)
     base.class_eval do
       has_many :unavailable_dates, dependent: :destroy
-    end
 
-    scope :available_for_date, ->(date) do
-      where('NOT EXISTS (SELECT unavailable_dates.* FROM unavailable_dates WHERE unavailable_dates.user_id = users.id AND (unavailable_dates.from <= ? AND unavailable_dates.to >= ?))', date, date)
+      scope :available_for_date, ->(date) do
+        where("NOT EXISTS (SELECT unavailable_dates.* FROM unavailable_dates WHERE unavailable_dates.#{base.to_s.downcase}_id = #{base.table_name}s.id AND (unavailable_dates.from <= ? AND unavailable_dates.to >= ?))", date, date)
+      end
     end
   end
 
